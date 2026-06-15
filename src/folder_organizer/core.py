f6e7792c41f7c529ec5bd1580666834bed35442a
Abdul Muhaymen Ali <abdulmuhaymenali@gmail.com>
2026-06-16 07:19:59 +0800

    feat: implement robust file-moving logic with extension check

--- src/folder_organizer/core.py
+++ src/folder_organizer/core.py
@@ -1,24 +1,42 @@
+import shutil
 from pathlib import Path
 
 def organize_folder(target_folder: str):
     # Convert given folder name into Path
     target_folder_path = Path.home() / target_folder 
+    
+    file_script = Path(__file__).resolve()
 
     try:
-        """
-        Grab the next item from iterator. It will route to except,
-        if folder doesn't exists, not a directory, or empty
-        """
-        next(target_folder_path.iterdir())
-        
+
         for content in target_folder_path.iterdir():
-            print(content.name)
-        
+            
+            if content.is_file():
+                
+                if content == file_script:
+                    continue
+                
+                elif content.name.startswith("."):
+                    continue
+                
+                else:
+                    
+                    if content.suffix == "":
+                        assign_folder = "no_extension"
+                    
+                    else:
+                        assign_folder = content.suffix[1:].lower()
+                    
+                    assign_folder_path = target_folder_path / assign_folder
+                    assign_folder_path.mkdir(exist_ok=True)                       
+                    
+                    old_content_path = target_folder_path / content.name
+                    new_content_path = target_folder_path / assign_folder / content.name
+                    
+                    shutil.move(str(old_content_path), str(new_content_path))
+                        
     except FileNotFoundError as e:
         print(f"[{type(e).__name__}]: {target_folder_path} -> {e.strerror}")
-        
-    except StopIteration:
-        print(f"'{target_folder_path.name}' contains zero items")
     
     except NotADirectoryError as e:
         print(f"[{type(e).__name__}]: '{target_folder_path.name}' is a file, not a folder")
